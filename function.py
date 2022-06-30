3322e6bde2891c4cd90becebbcdf5bee412a27bb
Krrish77 <83108318+Krrish77@users.noreply.github.com>
2022-06-29 21:33:57 -0700

    Update function.py
    update the function

--- function.py
+++ function.py
@@ -4,7 +4,8 @@ def lambda_handler(event, context):
     # TODO implement
     
     print("Execution started")
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from lambda')
+        'body': json.dumps('Hello from Gitgihub')
     }
